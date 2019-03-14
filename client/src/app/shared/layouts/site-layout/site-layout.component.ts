4a53310e9d0810a1e92b30a03f35c6ace55c7d71
icecroc <icecroc@hotmail.com>
2019-03-14 12:33:45 +0500

    yeah jalap

---
+++ client/src/app/shared/layouts/site-layout/site-layout.component.ts
@@ -0,0 +1,33 @@
+import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core'
+import {AuthService} from '../../services/auth.service'
+import {Router} from '@angular/router'
+import {MaterialService} from '../../classes/material.service'
+
+@Component({
+  selector: 'app-site-layout',
+  templateUrl: './site-layout.component.html',
+  styleUrls: ['./site-layout.component.css']
+})
+export class SiteLayoutComponent  {
+
+  
+
+  links = [
+    {url: '/history', name: 'Архив'},
+    {url: '/closed', name: 'Закрытые заявки'},
+    {url: '/answer', name: 'Открытые заявки'},
+    {url: '/order', name: 'Добавить заявку'},
+    {url: '/categories', name: 'Шаблоны'}
+  ]
+
+  constructor(private auth: AuthService,
+              private router: Router) {
+  }
+
+  logout(event: Event) {
+    event.preventDefault()
+    this.auth.logout()
+    this.router.navigate(['/login'])
+  }
+
+}
---
+++ routes/order.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const passport = require('passport')
+const controller = require('../controllers/order')
+const router = express.Router()
+
+
+router.get('/', passport.authenticate('jwt', {session: false}), controller.getAll)
+router.get('/closed', passport.authenticate('jwt', {session: false}), controller.getClosed)
+router.get('/archived', passport.authenticate('jwt', {session: false}), controller.getArchived)
+router.post('/', passport.authenticate('jwt', {session: false}), controller.create)
+router.post('/:id', passport.authenticate('jwt', {session: false}),controller.update)
+
+
+module.exports = router
